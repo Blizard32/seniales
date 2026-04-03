e56611a119cb4a3d6953eb933e70f3ad834f7818
Blizard32 <fouteiral32@gmail.com>
2026-04-03 14:40:20 -0300

    Probando al cambiar el __init__

--- seniales/__init__.py
+++ seniales/__init__.py
@@ -1,5 +1,5 @@
-from signals import cajon
-from signals import triangulo
-from signals import new_time_dim
+from .signals import cajon
+from .signals import triangulo
+from .signals import new_time_dim
 
-from operaciones import correlacion
+from .operaciones import correlacion
